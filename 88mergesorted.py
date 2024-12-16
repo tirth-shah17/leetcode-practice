fbb231f417c014ef86d95320ebaa89fa8de301da
tirth-shah17 <128163167+tirth-shah17@users.noreply.github.com>
2024-12-16 22:27:43 +0530

    Merge Sorted Array (easy)

---
+++ 88mergesorted.py
@@ -0,0 +1,25 @@
+class Solution:
+    def merge(self,nums1,nums2,m,n):
+        last = m + n - 1
+        
+        while m > 0 and n > 0:
+            if nums1[m-1] > nums2[n-1]:
+                nums1[last] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[last] = nums2[n-1]
+                n -= 1
+            last -= 1
+            
+        while n > 0:
+            nums1[last] = nums2[n-1]
+            n,last = n-1,last-1
+            
+solution = Solution()
+
+nums1 = [1,2,3,0,0,0]
+nums2 = [2,5,6]
+m = 3
+n = 3
+solution.merge(nums1,nums2,m,n)
+print(nums1)
